a93072c1f85df746b0b8f773bdfb91fb2adc5edb
matheuscas <matheus.mcas@gmail.com>
2014-02-23 02:04:31 -0300

    pre_processing module refactoring

--- pre_processing.py
+++ pre_processing.py
@@ -1,5 +1,6 @@
 import string
 import nltk
+import pattern.en
 
 #stopwords from http://nlp.stanford.edu/IR-book/html/htmledition/dropping-common-terms-stop-words-1.html
 STANFORD_STOPWORDS = ["a", "an", "and", "are", "as", "at","be","by","for","from",
@@ -7,31 +8,47 @@ STANFORD_STOPWORDS = ["a", "an", "and", "are", "as", "at","be","by","for","from"
 						"to", "was","were", "will","with"]
 
 NLTK_STOPWORDS = nltk.corpus.stopwords.words('english')
+UNIGRAMS = 1
+BIGRAMS = 2
+TRIGRAMS = 3
 
 def tokenizer(raw_text):
-	"""Split a raw_text into a list of words"""
+	"""Split a raw_text into a list of words
+	Returns a string list of words without punctuation
+	"""
 
 	return __manual_tokenizer(raw_text)
 
-def stopwords_removal(tokenized_string, method=None):
-	"""Remove stopwords from a tokenized text using the choosed method
+def stopwords_removal(text, method=None):
+	"""Remove stopwords from text using the choosed method
 	If method is None, the STANFORD_STOPWORDS are used to remove them.
 	If method is 'nltk', the nltk lib does the stopwords removal.
 	"""
+
+	tokenized_string = tokenizer(text)
 
 	if not method:
 		return __manual_stopwords_removal(tokenized_string)
 	elif method == "nltk":
 		return __nltk_stopwords_removal(tokenized_string)
 
-def punctuation_removal(raw_text):
-	"""Remove punctuation from raw text"""
-
-	return __manual_punctuation_removal(raw_text)
-
-def pos_tagger(tokenized_string):
+def pos_tagger(tokenized_string, ngrams=None):
 	"""This function does the Part of Speech tagging, using nltk lib"""
+
 	return nltk.pos_tag(tokenized_string)
+
+def pos_tagger_pattern(raw_text, ngrams=UNIGRAMS, continuous=False):
+	"""This function does the Part of Speech tagging, using pattern lib.
+	It does not need the text to be tokenized first"""
+
+	tagged = pattern.en.parse(raw_text, chunks=False)
+	return pattern.en.ngrams(tagged, n=ngrams, continuous=continuous)
+
+def extract_ngrams(doc):
+
+	doc.unigrams = pos_tagger_pattern(doc.raw_text)
+	doc.bigrams = pos_tagger_pattern(doc.raw_text, ngrams=BIGRAMS)
+	doc.trigrams = pos_tagger_pattern(doc.raw_text, ngrams=TRIGRAMS)
 
 """PRIVATE FUNCTIONS"""
 
@@ -44,10 +61,13 @@ def __manual_punctuation_removal(raw_text):
 
 #tokenizer functions
 def __manual_tokenizer(string_text):
-	return string_text.split()
+
+	no_punctuation = __manual_punctuation_removal(string_text)
+	return no_punctuation.split()
 
 #stopwords_removal functions
 def __manual_stopwords_removal(tokenized_string):
+
 	tokenized_string_no_stopwords = [w for w in tokenized_string if w.lower() not in STANFORD_STOPWORDS]
 	return tokenized_string_no_stopwords
 
--- tests/test_pre_processing.py
+++ tests/test_pre_processing.py
@@ -9,38 +9,32 @@ import pre_processing as pp
 document_sample_text = """three lengthy court cases are portrayed with spielberg's
 							trademark panache - - flashy beginning , lots of facial close-ups ,
 							big music , and dramatic imagery."""
-document_sample_text_tokens = 26
-
-def test_punctuation_removal():
-	no_punctuation_text = pp.punctuation_removal(document_sample_text)
-	for p in string.punctuation:
-		assert p not in no_punctuation_text
+document_sample_text_tokens = 21 #without punctuation
 
 def test_tokenizer():
 	tokens = pp.tokenizer(document_sample_text)
 	assert len(tokens) == document_sample_text_tokens
 
 def test_stopwords_removal_stanford():
-	no_punctuation_text = pp.punctuation_removal(document_sample_text)
-	tokens = pp.tokenizer(document_sample_text)
-	tokens_no_stopwords = pp.stopwords_removal(tokens)
+
+	tokens_no_stopwords = pp.stopwords_removal(document_sample_text)
 
 	for t in tokens_no_stopwords:
 		assert t.lower() not in pp.STANFORD_STOPWORDS
 
 def test_stopwords_removal_nltk():
-	no_punctuation_text = pp.punctuation_removal(document_sample_text)
-	tokens = pp.tokenizer(document_sample_text)
-	tokens_no_stopwords = pp.stopwords_removal(tokens, method='nltk')
+
+	tokens_no_stopwords = pp.stopwords_removal(document_sample_text, method='nltk')
 
 	for t in tokens_no_stopwords:
 		assert t.lower() not in pp.NLTK_STOPWORDS
 
 def test_pos_tagger_nltk():
-	no_punctuation_text = pp.punctuation_removal(document_sample_text)
-	tokens = pp.tokenizer(document_sample_text)
-	tokens_no_stopwords = pp.stopwords_removal(tokens, method='nltk')
+
+	tokens_no_stopwords = pp.stopwords_removal(document_sample_text, method='nltk')
 	tokens_tagged = pp.pos_tagger(tokens_no_stopwords)
 
 	for t in tokens_tagged:
 		assert len(t) == 2 and t[1]
+
+
