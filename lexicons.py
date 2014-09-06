732df9e7519e69bcc3db3a1e0e6dbd241e15d974
matheuscas <matheus.mcas@gmail.com>
2014-09-06 01:36:21 -0300

    added SubjectivityClues lexicon from Wilson,2005

--- lexicons.py
+++ lexicons.py
@@ -14,7 +14,7 @@ class BaseLexicon(object):
 		return self.entries.find({'_id':ObjectId(entry_id)})[0]
 
 	def get_entry_by_name(self, entry_name, entry_field):
-		return self.entries.find({entry_field:entry_name})				
+		return self.entries.find({entry_field:entry_name})
 
 class SentiWords(BaseLexicon):
 	"""Class that models SentiWords from (Gerrine et al.,2013)"""
@@ -37,15 +37,36 @@ class SentiWords(BaseLexicon):
 
 		return list_of_dict_units	
 
+	def get_entry_by_name(self, entry_name , entry_field='lemma'):
+		return super(SentiWords, self).get_entry_by_name(entry_name, entry_field)
+
+class SubjectivityClues(BaseLexicon):
+	"""Class that models SubjectivityClues from (Wilson et al.,2005)"""
+
+	def __init__(self, lexicon_name="SubjectivityClues"):
+		BaseLexicon.__init__(self, lexicon_name)
+						
+	def __read_lexicon_source(self):
+		path_file = "lexicons/SubjectivityClues/subjclueslen1-HLTEMNLP05.tff"
+		file_obj = open(path_file,"r")
+
+		list_of_dict_units = []
+		for line in file_obj.readlines():
+			clue = line.split()
+			_type, _len, word1, pos1, stemmed1, priorpolarity = clue[0].split("=")[1], clue[1].split("=")[1], clue[2].split("=")[1], clue[3].split("=")[1], clue[4].split("=")[1], clue[5].split("=")[1]
+			list_of_dict_units.append({"type":_type, "len":_len, "word1":word1, "pos1":pos1, "stemmed1":stemmed1, "priorpolarity":priorpolarity})
+
+		return list_of_dict_units
+
 	def create_database(self):
 		entries = self.__read_lexicon_source()
 		self.entries.drop()
 
 		for entry in entries:
-			self.entries.insert(entry)
+			self.entries.insert(entry)					
 
-	def get_entry_by_name(self, entry_name , entry_field='lemma'):
-		return super(SentiWords, self).get_entry_by_name(entry_name, entry_field)
+	def get_entry_by_name(self, entry_name , entry_field='word1'):
+		return super(SubjectivityClues, self).get_entry_by_name(entry_name, entry_field)						
 
 
 	
